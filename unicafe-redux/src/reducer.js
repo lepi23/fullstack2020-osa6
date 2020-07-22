35579fbe40505cc363d58790ef62d973180d5c34
Leevi Saarinen <leevi.saarinen@tuni.fi>
2020-07-22 15:25:23 +0300

    unicafe

---
+++ unicafe-redux/src/reducer.js
@@ -0,0 +1,33 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0,
+  all: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'GOOD':
+      const newGood = {
+        ...state, good: state.good + 1, all: state.all + 1
+      }
+      return newGood
+    case 'OK':
+      const newOk = {
+        ...state, ok: state.ok + 1, all: state.all + 1
+      }
+      return newOk
+    case 'BAD':
+      const newBad = {
+        ...state, bad: state.bad + 1, all: state.all + 1
+      }
+      return newBad
+    case 'ZERO':
+      return initialState
+    default: return state
+  }
+  
+}
+
+export default counterReducer
